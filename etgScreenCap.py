a23d052f8e9dd64a7a734625f2c7a9484a1727bf
Matthew Kennedy <35405371+mrmattkennedy@users.noreply.github.com>
2019-01-11 22:44:26 -0500

    Initialized repo

---
+++ etgScreenCap.py
@@ -0,0 +1,15 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+
+last_time = time.time()
+while(True):
+    screen =  np.array(ImageGrab.grab(bbox=(0, 40, 640, 480)))
+    
+    print('Loop took {} seconds'.format(time.time()-last_time))
+    last_time = time.time()
+    cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
